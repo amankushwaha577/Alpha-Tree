09d3e4c9b17e5d72996cc50939afbe298d6c9be1
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-07-30 01:05:43 +0530

    easy

--- _105. Diameter of Binary Tree/src/Better.java
+++ _105. Diameter of Binary Tree/src/Better.java
@@ -43,6 +43,15 @@ public class Better {
 
     // 🔬 Test
     public static void main(String[] args) {
+        /*
+                     1
+                    / \
+                   2   3
+                  / \
+                 4   5
+                /
+               6
+         */
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
